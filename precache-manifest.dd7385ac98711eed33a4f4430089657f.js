74f21aa6b7dbec4dd6c88fd0cc04da0bd173051b
qyao4 <138896505+qyao4@users.noreply.github.com>
2024-03-05 00:26:29 -0600

    Updates

---
+++ precache-manifest.dd7385ac98711eed33a4f4430089657f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ece4add82b94cf374361d8eac92df10",
+    "url": "/web3012_coding_assignment14/index.html"
+  },
+  {
+    "revision": "2750761e20659b600f4a",
+    "url": "/web3012_coding_assignment14/static/css/2.fd277b09.chunk.css"
+  },
+  {
+    "revision": "b6fd364a0cded7419122",
+    "url": "/web3012_coding_assignment14/static/css/main.df700d1f.chunk.css"
+  },
+  {
+    "revision": "2750761e20659b600f4a",
+    "url": "/web3012_coding_assignment14/static/js/2.6b8ed305.chunk.js"
+  },
+  {
+    "revision": "46a379a572333860f78765c743603e43",
+    "url": "/web3012_coding_assignment14/static/js/2.6b8ed305.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b6fd364a0cded7419122",
+    "url": "/web3012_coding_assignment14/static/js/main.96879bbb.chunk.js"
+  },
+  {
+    "revision": "9fec7ffd92372773108a",
+    "url": "/web3012_coding_assignment14/static/js/runtime-main.f4ffc4c9.js"
+  }
+]);
